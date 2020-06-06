5e39edd93f694cbcddeca0ad6cf0ca805e52c690
Dhaval <gohel.dhaval24@gmail.com>
2020-06-06 15:35:02 +0530

    Integrate mongodb

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -5,19 +5,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Login_model extends CI_Model {
 
 	public function getAdminLogin($email, $pwd) {
-		$this->db->where('email_id', $email);
-		$this->db->where('password', $pwd);
-		return $this->gdb->get($this->common->getAdminTable())->row();
+		$this->mongo_db->where('email_id', $email);
+		$this->mongo_db->where('password', $pwd);
+		return $this->mongo_db->select($this->common->getAdminTable())->row();
 	}
 
 	public function getUserLogin($email, $pwd) {
-		$this->db->where('email', $email);
-		$this->db->where('password', $pwd);
-		return $this->gdb->get($this->common->getUserTable())->row();
+		$this->mongo_db->where('email_id', $email);
+		$this->mongo_db->where('password', $pwd);
+		return $this->mongo_db->get($this->common->getAdminTable());
+		// $this->mongo_db->where('email', $email);
+		// $this->mongo_db->where('password', $pwd);
+		// return $this->gdb->get($this->common->getUserTable())->row();
 	}
 
 	public function updatePassword($id, $data) {
-		$this->db->where('admin_id', $id);
-		$this->db->update($this->common->getAdminTable(), $data);
+		$this->mongo_db->where('admin_id', $id);
+		$this->mongo_db->update($this->common->getAdminTable(), $data);
 	}
 }
